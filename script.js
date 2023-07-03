660c51de414ad2f8978f5dab2487093fb400fbaf
Kendall Robinson <kentechs91@gmail.com>
2023-07-03 16:04:45 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -6,62 +6,67 @@
 //Write filterByGenre(books, genre) below here:
 
 
-const books = [
-  {
-    title: "The Da Vinci Code",
-    author: "Dan Brown",
-    genre: "mystery",
-  },
-  {
-    title: "Pride and Prejudice",
-    author: "Jane Austen",
-    genre: "romance",
-  },
-  {
-    title: "The Hitchhiker's Guide to the Galaxy",
-    author: "Douglas Adams",
-    genre: "sci-fi",
-  },
-  {
-    title: "The Girl with the Dragon Tattoo",
-    author: "Stieg Larsson",
-    genre: "mystery",
-  },
-  {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    genre: "romance",
-  },
-  {
-    title: "Ender's Game",
-    author: "Orson Scott Card",
-    genre: "sci-fi",
-  },
-  {
-    title: "The Silence of the Lambs",
-    author: "Thomas Harris",
-    genre: "mystery",
-  },
-  {
-    title: "Gone with the Wind",
-    author: "Margaret Mitchell",
-    genre: "romance",
-  },
-  {
-    title: "Dune",
-    author: "Frank Herbert",
-    genre: "sci-fi",
-  },
-  {
-    title: "The Big Sleep",
-    author: "Raymond Chandler",
-    genre: "mystery",
-  },
-  {
-    title: "Jane Eyre",
-    author: "Charlotte Bronte",
-    genre: "romance",
-  },
-];
+  const books = [
+    {
+      title: "The Da Vinci Code",
+      author: "Dan Brown",
+      genre: "mystery",
+    },
+    {
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      genre: "romance",
+    },
+    {
+      title: "The Hitchhiker's Guide to the Galaxy",
+      author: "Douglas Adams",
+      genre: "sci-fi",
+    },
+    {
+      title: "The Girl with the Dragon Tattoo",
+      author: "Stieg Larsson",
+      genre: "mystery",
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      genre: "romance",
+    },
+    {
+      title: "Ender's Game",
+      author: "Orson Scott Card",
+      genre: "sci-fi",
+    },
+    {
+      title: "The Silence of the Lambs",
+      author: "Thomas Harris",
+      genre: "mystery",
+    },
+    {
+      title: "Gone with the Wind",
+      author: "Margaret Mitchell",
+      genre: "romance",
+    },
+    {
+      title: "Dune",
+      author: "Frank Herbert",
+      genre: "sci-fi",
+    },
+    {
+      title: "The Big Sleep",
+      author: "Raymond Chandler",
+      genre: "mystery",
+    },
+    {
+      title: "Jane Eyre",
+      author: "Charlotte Bronte",
+      genre: "romance",
+    },
+  ];
 
+  let filterByGenre = books.filter(function(books){
+        return books.genre === "romance"
+  })
+  
 
+console.log(filterByGenre);
